ff3468011cffbec688ce19de553af471bcb4b866
yaimai289 <yaimai3153@gmail.com>
2023-10-10 22:28:31 +0800

    10.10 type

--- mysh/builtin/__init__.py
+++ mysh/builtin/__init__.py
@@ -3,7 +3,7 @@ from mysh.builtin.exit import exit
 from mysh.builtin.alias import alias
 from mysh.builtin.which import which
 from mysh.builtin.history import history, save_history
-from mysh.builtin.ehco import echo
+from mysh.builtin.echo import echo
 from mysh.builtin.pwd import pwd
 from mysh.builtin.solve_home_dir import solve_home_dir
 from mysh.builtin.type import type
